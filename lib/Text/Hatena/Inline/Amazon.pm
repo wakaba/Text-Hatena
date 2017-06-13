eaa8f8f495fa9452a88194bbb1835b3a2aa5c256
Wakaba <wakaba@suikawiki.org>
2017-06-13 20:09:03 +0900

    Changed Amazon URLs to https: and made associate ID configurable using constructor option

--- lib/Text/Hatena/Inline/Amazon.pm
+++ lib/Text/Hatena/Inline/Amazon.pm
@@ -9,10 +9,10 @@ build_inlines {
     # [amazon:___]
     syntax qr{\[amazon:(.+)\]}i=> sub {
         my ($context, $keyword) = @_;
-        my $amazonid = $context->stash->{affiliateid}->{amazonid} || 'hatena-22';
+        my $amazonid = $context->{amazonid} || $context->stash->{affiliateid}->{amazonid} || 'hatena-22';
         my $link_target = $context->link_target;
         my $escaped_keyword = uri_escape_utf8($keyword);
-        return qq|<a href="http://www.amazon.co.jp/exec/obidos/external-search?mode=blended&tag=$amazonid&keyword=$escaped_keyword"$link_target>amazon:$keyword</a>|;
+        return qq|<a href="https://www.amazon.co.jp/exec/obidos/external-search?mode=blended&tag=$amazonid&keyword=$escaped_keyword"$link_target>amazon:$keyword</a>|;
     };
 };
 
--- t/52_amazon.t
+++ t/52_amazon.t
@@ -18,4 +18,4 @@ __END__
 --- input
 [amazon:魔法少女まどか☆マギカ]
 --- expected
-<p><a target="_blank" href="http://www.amazon.co.jp/exec/obidos/external-search?mode=blended&amp;tag=hatena-22&amp;keyword=%E9%AD%94%E6%B3%95%E5%B0%91%E5%A5%B3%E3%81%BE%E3%81%A9%E3%81%8B%E2%98%86%E3%83%9E%E3%82%AE%E3%82%AB">amazon:魔法少女まどか☆マギカ</a></p>
+<p><a target="_blank" href="https://www.amazon.co.jp/exec/obidos/external-search?mode=blended&amp;tag=hatena-22&amp;keyword=%E9%AD%94%E6%B3%95%E5%B0%91%E5%A5%B3%E3%81%BE%E3%81%A9%E3%81%8B%E2%98%86%E3%83%9E%E3%82%AE%E3%82%AB">amazon:魔法少女まどか☆マギカ</a></p>
